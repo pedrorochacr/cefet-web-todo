f47d8a412cd5f97276b1a7a311bd157d217e8535
Pedro Rocha <84788885+pedrorochacr@users.noreply.github.com>
2026-04-29 09:48:17 -0300

    Adicionando tarefa

--- tarefas.js
+++ tarefas.js
@@ -2,3 +2,19 @@ const tarefas = [
   { nome: 'Comprar leite', categoria: 'compras', realizada: false },
   { nome: 'Escutar chimbinha', categoria: 'lazer', realizada: true },
 ];
+
+function insereTarefaNaPagina(tarefa) {
+  const lista = document.querySelector('#lista-tarefas');
+  const item = document.createElement('li');
+
+  item.classList.add('item-tarefa');
+  if (tarefa.realizada) {
+    item.classList.add('marcado');
+  }
+  item.dataset.categoria = tarefa.categoria;
+  item.textContent = tarefa.nome;
+
+  lista.appendChild(item);
+}
+
+tarefas.forEach(insereTarefaNaPagina);
